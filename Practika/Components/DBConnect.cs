c12e2123f4da257b664f0467aa070ce153ce489e
cbvfgtn217@gmail.com <cbvfgtn217@gmail.com>
2022-12-01 15:25:31 +0300

    Добавьте файлы проекта.

---
+++ Practika/Components/DBConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practika.Components
+{
+    internal class DBConnect
+    {
+        public static ProductsSessionEntities2 db = new ProductsSessionEntities2();
+    }
+}
