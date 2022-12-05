e746b2b567e6297a056b8be488c0b3dd80040216
cbvfgtn217@gmail.com <cbvfgtn217@gmail.com>
2022-12-05 19:53:39 +0300

    8

--- Practika/Pages/AddEditProductPage.xaml.cs
+++ Practika/Pages/AddEditProductPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Practika.Components;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,29 @@ namespace Practika.Pages
     /// </summary>
     public partial class AddEditProductPage : Page
     {
+        public ObservableCollection<Product> Products
+        {
+            get { return (ObservableCollection<Product>)GetValue(ProductsProperty); }
+            set { SetValue(ProductsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ProductsProperty =
+            DependencyProperty.Register("Products", typeof(ObservableCollection<Product>), typeof(ProductsListPage));
+
         public AddEditProductPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
