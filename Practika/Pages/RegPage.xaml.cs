4dfc5e0ee86672623d0e609da57f81780a5b288a
cbvfgtn217@gmail.com <cbvfgtn217@gmail.com>
2022-12-12 06:24:08 +0300

    lsst

--- Practika/Pages/RegPage.xaml.cs
+++ Practika/Pages/RegPage.xaml.cs
@@ -67,7 +67,7 @@ namespace Practika.Pages
                 }
                 else
                 {
-                    MessageBox.Show("Проверьте на правильность заполнения." +
+                    MessageBox.Show("Проверьте на правильность заполнения.\n" +
                         " Пароль должен содержать 6 символов, хотя бы 1 прописную букву," +
                         " хотя бы 1 цифру и хотя бы 1 из этих символов ! @ # $ % ^");
                 }
@@ -77,85 +77,8 @@ namespace Practika.Pages
                 MessageBox.Show("Такой пользователь уже существует");
             }
 
-            //string login = LoginTb.Text.Trim();
-            //string password = PasswordTb.Text.Trim();
-            //string firstname = FirstNameTb.Text.Trim();
-            //string lastname = LastNameTb.Text.Trim();
-            //string patronymic = PatronymicTb.Text.Trim();
-            //string phone = PhoneTb.Text.Trim();
-            //string email = EmailTb.Text.Trim();
-            //var check = DBConnect.db.User.Where(x => x.Login == login).FirstOrDefault();
-            //if (check == null)
-            //{
-            //    if (login.Length > 0 && password.Length > 0 && firstname.Length > 0 && lastname.Length > 0 && email.Length > 0)
-            //    {
-            //        if (DBConnect.db.User.Local.Any(x => x.Login == login && x.Email == email && x.Phone == phone))
-            //        {
-            //            MessageBox.Show("Tакой пользователь уже существует");
-            //            return;
-            //        }
-            //        else
-            //        {
-            //            DBConnect.db.User.Add(new User
-            //            {
-            //                Login = login,
-            //                Password = password,
-            //                FirstName = firstname,
-            //                LastName = lastname,
-            //                Patronymic = patronymic,
-            //                Phone = phone,
-            //                Email = email,
-            //                GenderId = GenderCb.SelectedIndex + 1,
-            //                RoleId = 1
-            //            });
-            //            if (password.Length >= 6)
-            //            {
-            //                bool symbol = false;
-            //                bool number = false;
-            //                bool IsAllUpper = false;
-            //                for (int i = 0; i < password.Length; i++)
-            //                {
-            //                    if (password[i] >= '0' && password[i] <= '9') number = true;
-            //                    if (password[i] == '!' || password[i] == '@' || password[i] == '#' || password[i] == '%' || password[i] == '^') symbol = true;
-            //                    if (Char.IsUpper(password[i])) IsAllUpper = true;
-            //                }
-            //                if (!symbol)
-            //                {
-            //                    MessageBox.Show("Добавьте один из следующих символов: ! @ # $ % ^");
-            //                    return;
-            //                }
-            //                else if (!number)
-            //                {
-            //                    MessageBox.Show("Добавьте хотя бы одну цифру");
-            //                    return;
-            //                }
-            //                else if (!IsAllUpper)
-            //                {
-            //                    MessageBox.Show("Добавьте одну прописную букву");
-            //                    return;
-            //                }
-
-            //                MessageBox.Show("Регистрация прошла успешно");
-            //                DBConnect.db.SaveChanges();
-            //                Navigation.BackPage();
-            //            }
-            //            else
-            //            {
-            //                MessageBox.Show("Пароль слишком короткий, требуется минимум 6 символов!");
-            //                return;
-            //            }
-
-
-            //        }
-
-            //    }
-            //    else
-            //    {
-            //        MessageBox.Show("Заполните поля");
-            //        return;
-            //    }
-
-            //}
+            
+            
 
 
         }
